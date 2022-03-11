f1ff5b628fac52cab6a82112f375cfac5f37ad8f
Codelleschi <a.steinhauslin@gmail.com>
2022-03-11 08:34:13 -0800

    added Home content

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section>
+      <h1 class="welcome__title">
+        My name is Alex and I am a Front-End Web Developer
+      </h1>
+      <div class="welcome__text">
+        <p>
+          I have a passion for coding and I love spending my free time
+          developing
+        </p>
+        <p>beautiful static websites and functional React apps.</p>
+      </div>
+      <a
+        href="https://www.linkedin.com/in/alexandre-steinhauslin/"
+        class="welcome__button"
+      >
+        linkedin
+      </a>
+    </section>
+  );
+};
+
+export default Home;
